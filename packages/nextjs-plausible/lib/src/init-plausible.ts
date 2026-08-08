140df4016dfa2a0cd6851bf7978693b45f8e729a
nikolas.rimikis <nikolas.rimikis@fold.land>
2026-08-08 16:22:01 +0000

    fix(nextjs-plausible): add explicit initPlausible return types
    Signed-off-by: nikolas.rimikis <nikolas.rimikis@fold.land>

--- packages/nextjs-plausible/lib/src/init-plausible.ts
+++ packages/nextjs-plausible/lib/src/init-plausible.ts
@@ -6,7 +6,7 @@ import type { NextEnv } from './env'
 export function initPlausible(
   env: Pick<NextEnv, 'PLAUSIBLE_PROXY_ENDPOINT'>,
   config?: Omit<PlausibleConfig, 'endpoint'>
-): void {
+): ReturnType<typeof init> {
   init({
     domain: globalThis.location.hostname,
     endpoint: env.PLAUSIBLE_PROXY_ENDPOINT,
