ea7656e8895ea2efcb1535f8b31c61d36c9c2285
nikolas.rimikis <nikolas.rimikis@fold.land>
2026-08-08 18:15:33 +0000

    fix(nextjs-plausible): bundle bugged @plausible-analytics/tracker
    Signed-off-by: nikolas.rimikis <nikolas.rimikis@fold.land>

--- packages/nextjs-plausible/lib/src/init-plausible.ts
+++ packages/nextjs-plausible/lib/src/init-plausible.ts
@@ -1,19 +1,21 @@
 import 'client-only'
 
-import { init, type PlausibleConfig } from '@plausible-analytics/tracker'
+import type { init, PlausibleConfig } from '@plausible-analytics/tracker'
 import type { NextEnv } from './env'
 
 export function initPlausible(
   env: Pick<NextEnv, 'PLAUSIBLE_PROXY_ENDPOINT'>,
   config?: Omit<PlausibleConfig, 'endpoint'>
 ): ReturnType<typeof init> {
-  init({
-    domain: globalThis.location.hostname,
-    endpoint: env.PLAUSIBLE_PROXY_ENDPOINT,
-    autoCapturePageviews: true,
-    outboundLinks: true,
-    formSubmissions: true,
-    bindToWindow: false,
-    ...config,
+  import('@plausible-analytics/tracker').then(({ init }) => {
+    init({
+      domain: globalThis.location.hostname,
+      endpoint: env.PLAUSIBLE_PROXY_ENDPOINT,
+      autoCapturePageviews: true,
+      outboundLinks: true,
+      formSubmissions: true,
+      bindToWindow: false,
+      ...config,
+    })
   })
 }
--- packages/nextjs-plausible/lib/src/track.ts
+++ packages/nextjs-plausible/lib/src/track.ts
@@ -1,13 +1,12 @@
 import 'client-only'
 
-import { track as trackPlausible } from '@plausible-analytics/tracker'
+import type { track as trackPlausible } from '@plausible-analytics/tracker'
 
 /** Tracks an event, requires `initPlausible` to be called first. */
 export function track(
   ...props: Parameters<typeof trackPlausible>
 ): ReturnType<typeof trackPlausible> {
-  // biome-ignore lint/style/useGlobalThis: If not checked the prerender will call track and fail
-  if (typeof window !== 'undefined') {
-    trackPlausible(...props)
-  }
+  import('@plausible-analytics/tracker').then(({ track }) => {
+    track(...props)
+  })
 }
